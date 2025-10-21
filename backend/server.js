2206b13fa8a924679bc379edb4010638662985ca
Dragon <126907003+gnanadeepakvardhan@users.noreply.github.com>
2025-10-21 14:01:28 +0530

    Move URL shortening logic to urlRoutes
    Refactor URL shortening logic into separate routes.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Url = require('./models/Url');
-const shortid = require('shortid');
+const urlRoutes = require('./routes/url');
 const app = express();
 const PORT = process.env.PORT || 5000;
 require('dotenv').config();
@@ -16,23 +15,7 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
-app.post('/shorten', async (req, res) => {
-  const { longUrl } = req.body;
-  const shortUrl = shortid.generate();
-  const url = new Url({ longUrl, shortUrl });
-  await url.save();
-  res.json({ shortUrl: `${req.headers.host}/${shortUrl}` });
-});
-
-app.get('/:shortUrl', async (req, res) => {
-  const url = await Url.findOne({ shortUrl: req.params.shortUrl });
-  if (url) {
-    url.clicks++;
-    await url.save();
-    return res.redirect(url.longUrl);
-  }
-  res.status(404).json('URL not found');
-});
+// Use routes
+app.use('/', urlRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
