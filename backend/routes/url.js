da455db002e3796746d211181f291c01ccc3535b
Dragon <126907003+gnanadeepakvardhan@users.noreply.github.com>
2025-10-21 14:01:10 +0530

    Implement URL shortening and redirection routes

--- backend/routes/url.js
+++ backend/routes/url.js
@@ -1,1 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const Url = require('../models/Url');
+const shortid = require('shortid');
+
+// Shorten a URL
+router.post('/shorten', async (req, res) => {
+  const { longUrl } = req.body;
+  const shortUrl = shortid.generate();
+  const url = new Url({ longUrl, shortUrl });
+  await url.save();
+  res.json({ shortUrl: `${req.headers.host}/${shortUrl}` });
+});
+
+// Redirect from short URL to long URL
+router.get('/:shortUrl', async (req, res) => {
+  const url = await Url.findOne({ shortUrl: req.params.shortUrl });
+  if (url) {
+    url.clicks++;
+    await url.save();
+    return res.redirect(url.longUrl);
+  }
+  res.status(404).json('URL not found');
+});
+
+module.exports = router;
 
